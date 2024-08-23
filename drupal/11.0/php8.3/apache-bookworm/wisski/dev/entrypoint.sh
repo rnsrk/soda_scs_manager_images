0c24fdf94c5158364df262d620e38c135d5369c7
rnsrk <robert@nasarek.org>
2024-08-23 15:47:32 +0200

    add default graph and error output

--- drupal/11.0/php8.3/apache-bookworm/wisski/dev/entrypoint.sh
+++ drupal/11.0/php8.3/apache-bookworm/wisski/dev/entrypoint.sh
@@ -32,19 +32,19 @@ else
     --site-name="${SITE_NAME}" \
     --account-name="${DRUPAL_USER}" \
     --account-pass="${DRUPAL_PASSWORD}"
-  } &> /dev/null
+  } > /dev/null
   echo -e "\033[0;32mDRUPAL SITE \"${SITE_NAME}\" INSTALLED.\033[0m\n"
 
   # Lets get dirty with composer
   echo -e "\033[0;33mSET COMPOSER MINIMUM STABILITY.\033[0m"
-  composer config minimum-stability dev &> /dev/null
+  composer config minimum-stability dev > /dev/null
   echo -e "\033[0;32mCOMPOSER MINIMUM STABILITY SET.\033[0m\n"
   # Install development modules
   echo -e "\033[0;33mINSTALL DEVELOPMENT MODULES.\033[0m"
   {
     composer require drupal/devel
     drush en devel -y
-  } &> /dev/null
+  } > /dev/null
   echo -e "\033[0;32mDEVELOPMENT MODULES INSTALLED.\033[0m\n"
 
   # Add Drupal Recipe Composer plugin
@@ -53,7 +53,7 @@ else
     composer config repositories.ewdev vcs https://gitlab.ewdev.ca/yonas.legesse/drupal-recipe-unpack.git
     composer config allow-plugins.ewcomposer/unpack true
     composer require ewcomposer/unpack:dev-master
-  } &> /dev/null
+  } > /dev/null
   echo -e "\033[0;32mRECIPE COMPOSER PLUGIN INSTALLED.\033[0m\n"
 
   # Apply WissKI recipe
@@ -63,7 +63,7 @@ else
     drush recipe ../recipes/wisski_recipe_base_dev
     composer unpack rnsrk/wisski_recipe_base_dev 
     drush cr
-  } &> /dev/null
+  } > /dev/null
   echo -e "\033[0;32mWISSKI DEV RECIPE APPLIED.\033[0m\n"
 
   # Install default adapter
@@ -81,8 +81,8 @@ else
     --read_url=${TS_READ_URL} \
     --write_url=${TS_WRITE_URL} \
     --federatable=TRUE \
-    --default_graph="${DOMAIN}/contents/" \
-    --same_as="http://www.w3.org/2002/07/owl#sameAs" &> /dev/null
+    --default_graph=${DEFAULT_GRAPH} \
+    --same_as="http://www.w3.org/2002/07/owl#sameAs" > /dev/null
   echo -e "\033[0;32mDEFAULT TRIPLESTORE ADAPTER INSTALLED.\033[0m\n"
 
   # Set permissions
@@ -90,7 +90,7 @@ else
   {
     chown -R www-data:www-data /opt/drupal
     chmod -R 775 /opt/drupal
-  } &> /dev/null
+  } > /dev/null
   echo -e "\033[0;32mPERMISSIONS SET.\033[0m\n"
 fi
 echo -e "\033[0;32m+---------------------------+\033[0m"
